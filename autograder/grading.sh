aaaab52b489fb46ca9906aec993f6cd3df15daaa
Ngan <nguy4068@umn.edu>
2022-02-26 21:51:29 -0600

    modified command script for autograder

--- autograder/grading.sh
+++ autograder/grading.sh
@@ -51,28 +51,24 @@ done < machine.txt
 
 echo $setenvstring
 
-#Get the user ID
-
-USERID=`whoami`
-
 #Start the compute node 0
 CA=`echo "$setenvstring  cd $PROJ_PATH &&  nohup $NODE_COMMAND_0 & "`
-echo "ssh -t -n -f $USERID@$NODE_0_IP  \"csh -c '$CA'\" > /dev/null"  > command0.sh
+echo "ssh -t -n -f ramkr004@$NODE_0_IP  \"csh -c '$CA'\" > /dev/null"  > command0.sh
 source command0.sh
 
 #Start the compute node 1
 CA=`echo "$setenvstring cd $PROJ_PATH &&  nohup $NODE_COMMAND_1 & "`
-echo "ssh -t -n -f $USERID@$NODE_1_IP  \"csh -c '$CA'\" > /dev/null"  > command1.sh
+echo "ssh -t -n -f ramkr004@$NODE_1_IP  \"csh -c '$CA'\" > /dev/null"  > command1.sh
 source command1.sh
 
 #Start the compute node 2
 CA=`echo "$setenvstring cd $PROJ_PATH &&  nohup $NODE_COMMAND_2 & "`
-echo "ssh -t -n -f $USERID@$NODE_2_IP  \"csh -c '$CA'\" > /dev/null"  > command2.sh
+echo "ssh -t -n -f ramkr004@$NODE_2_IP  \"csh -c '$CA'\" > /dev/null"  > command2.sh
 source command2.sh
 
 #Start the compute node 3
 CA=`echo "$setenvstring cd $PROJ_PATH && nohup $NODE_COMMAND_3 & "`
-echo "ssh -t -n -f $USERID@$NODE_3_IP  \"csh -c '$CA'\" > /dev/null"  > command3.sh
+echo "ssh -t -n -f ramkr004@$NODE_3_IP  \"csh -c '$CA'\" > /dev/null"  > command3.sh
 source command3.sh
 
 # Sleep for a few seconds so the servers can startup
@@ -82,7 +78,7 @@ echo "The compute nodes should have booted up by now"
 
 #Start the server
 CA=`echo "$setenvstring cd $PROJ_PATH &&  nohup $SERVER_COMMAND & "`
-echo "ssh -t -n -f $USERID@$SERVER_IP  \"csh -c '$CA'\" > /dev/null"  > command4.sh
+echo "ssh -t -n -f ramkr004@$SERVER_IP  \"csh -c '$CA'\" > /dev/null"  > command4.sh
 source command4.sh
 
 # Sleep for a few seconds so the server can startup 
@@ -92,19 +88,19 @@ echo "The server should have booted up by now"
 
 #Start the client. It should automatically send the request
 CA=`echo "$setenvstring cd $PROJ_PATH && nohup $CLIENT_COMMAND & "`
-echo "ssh -t -n -f $USERID@$CLIENT_IP  \"csh -c '$CA'\" > /dev/null"  > command5.sh
+echo "ssh -t -n -f ramkr004@$CLIENT_IP  \"csh -c '$CA'\" > /dev/null"  > command5.sh
 source command5.sh
 
 ##### Assign Points ####
-echo "ssh -t -n -f $USERID@$SERVER_IP  ' ps -ef  > log_0  ' "  > grade0.sh
-echo "ssh -t -n -f $USERID@$CLIENT_IP  ' ps -ef > log_1  ' "  > grade1.sh
-echo "ssh -t -n -f $USERID@$NODE_0_IP  ' ps -ef  > log_2  ' "  > grade2.sh
-echo "ssh -t -n -f $USERID@$NODE_1_IP  ' ps -ef  > log_3  ' "  > grade3.sh
-echo "ssh -t -n -f $USERID@$NODE_2_IP  ' ps -ef > log_4  ' "  > grade4.sh
-echo "ssh -t -n -f $USERID@$NODE_3_IP  ' ps -ef > log_5  ' "  > grade5.sh
+echo "ssh -t -n -f ramkr004@$SERVER_IP  ' ps -ef  > log_0  ' "  > grade0.sh
+echo "ssh -t -n -f ramkr004@$CLIENT_IP  ' ps -ef > log_1  ' "  > grade1.sh
+echo "ssh -t -n -f ramkr004@$NODE_0_IP  ' ps -ef  > log_2  ' "  > grade2.sh
+echo "ssh -t -n -f ramkr004@$NODE_1_IP  ' ps -ef  > log_3  ' "  > grade3.sh
+echo "ssh -t -n -f ramkr004@$NODE_2_IP  ' ps -ef > log_4  ' "  > grade4.sh
+echo "ssh -t -n -f ramkr004@$NODE_3_IP  ' ps -ef > log_5  ' "  > grade5.sh
 
 echo "Waiting 60s for logging to complete ..."
-sleep 600
+sleep 360
 score=0
 for i in 0 1 2 3 4 5 
 do
