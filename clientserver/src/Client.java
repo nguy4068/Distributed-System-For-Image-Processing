ba2e6c0e7d20d527492eb19f10986eb535a545fa
Joon Kim <kim00967@umn.edu>
2022-02-28 00:42:14 -0600

    Allows the client to read in from a config file
    This commit modifies `build.xml` and the client code to accept a file and
    catches the server addrress from the file. If no server IP exists, then use
    localhost as the IP address.

--- clientserver/src/Client.java
+++ clientserver/src/Client.java
@@ -4,13 +4,17 @@ import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 import java.util.Scanner;
+import java.io.*;
 import server.*;
 public class Client {
 	public static Scanner scanner = new Scanner(System.in);
 	public static void main(String[] args){
 	try {
+
+    String serverAddr = getServerAddr(args[0]);
+    System.out.println(serverAddr);
 		TTransport transport;
-		transport = new TSocket("kh4250-09.cselabs.umn.edu",9000);
+		transport = new TSocket(serverAddr, 9000);
 		transport.open();
 		TProtocol protocol = new TBinaryProtocol(transport);
 		ServerService.Client client = new ServerService.Client(protocol);
@@ -33,5 +37,36 @@ public class Client {
 			
 
 	}
+
+
+  // A helper function to get the server IP address from a config file
+  // the argument is set in the build.xml
+  private static String getServerAddr(String file_path) {
+    // Based on Ngan's implementation
+    File file = new File(file_path);
+    Scanner scanner;
+    try {
+      scanner = new Scanner(file);
+    } catch (FileNotFoundException e) {
+      scanner = null;
+      System.out.println("machine file path not found");
+      // If no machine file is found, set the IP address to localhost
+      return "localhost";
+    }
+    while (scanner.hasNext()) {
+      String machine_config = scanner.nextLine();
+      String[] info = machine_config.split(" ");
+      String role = info[0];
+      String IP = info[1];
+      System.out.println("role: " + role);
+      // check if it's compute node configuration
+      if (role.startsWith("server")) {
+        System.out.println("IP: " + IP);
+        return IP;
+      }
+    }
+    // The file was there but the server IP was not. So just return localhost
+    return "localhost";
+  }
 	
 }
