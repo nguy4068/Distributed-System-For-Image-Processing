fcbdb23f6faed0748ee27b8a5958fa31e27d1f3e
Joon Kim <kim00967@umn.edu>
2022-02-28 02:28:08 -0600

    Update the autograder files
    The updated version from the TA

--- autograder/grading.sh
+++ autograder/grading.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
+# source ../setenv.sh
+
 count=0
 while read p; do
    if [ $count == 0  ]; then
-      NODE_0_PROGRAM=`echo $p | cut -f 1 -d ' '`
-      NODE_0_COMMAND=`echo $p`
+      NODE_COMMAND_0=`echo $p | cut --complement -f 1 -d' '`
    elif [ $count == 1  ]; then
-      NODE_1_PROGRAM=`echo $p | cut -f 1 -d ' '`
-      NODE_1_COMMAND=`echo $p`
+      NODE_COMMAND_1=`echo $p | cut --complement -f 1 -d' '`
    elif [ $count == 2  ]; then
-      NODE_2_PROGRAM=`echo $p | cut -f 1 -d ' '`
-      NODE_2_COMMAND=`echo $p`
+      NODE_COMMAND_2=`echo $p | cut --complement -f 1 -d' '`
    elif [ $count == 3  ]; then
-      NODE_3_PROGRAM=`echo $p | cut -f 1 -d ' '`
-      NODE_3_COMMAND=`echo $p`
+      NODE_COMMAND_3=`echo $p | cut --complement -f 1 -d' '`
    elif [ $count == 4  ]; then
-      SERVER_PROGRAM=`echo $p | cut -f 1 -d ' '`
-      SERVER_COMMAND=`echo $p`
+      SERVER_COMMAND=`echo $p | cut --complement -f 1 -d' '`
    elif [ $count == 5 ]; then
-      CLIENT_PROGRAM=`echo $p | cut -f 1 -d ' '`
-      CLIENT_COMMAND=`echo $p`
+      CLIENT_COMMAND=`echo $p | cut --complement -f 1 -d' '`
    fi 
    count=`echo "scale=0;$count + 1" | bc`
 done < commands.txt
@@ -57,24 +53,28 @@ done < machine.txt
 
 echo $setenvstring
 
+#Get the user ID
+
+USERID=`whoami`
+
 #Start the compute node 0
-CA=`echo "$setenvstring  cd $PROJ_PATH &&  nohup $NODE_0_COMMAND & "`
-echo "ssh -t -n -f nguy4068@$NODE_0_IP  \"csh -c '$CA'\" > /dev/null"  > command0.sh
+CA=`echo "$setenvstring  cd $PROJ_PATH &&  nohup $NODE_COMMAND_0 & "`
+echo "ssh -t -n -f $USERID@$NODE_0_IP  \"csh -c '$CA'\" > /dev/null"  > command0.sh
 source command0.sh
 
 #Start the compute node 1
-CA=`echo "$setenvstring cd $PROJ_PATH &&  nohup $NODE_1_COMMAND & "`
-echo "ssh -t -t -n -f nguy4068@$NODE_1_IP  \"csh -c '$CA'\" > /dev/null"  > command1.sh
+CA=`echo "$setenvstring cd $PROJ_PATH &&  nohup $NODE_COMMAND_1 & "`
+echo "ssh -t -n -f $USERID@$NODE_1_IP  \"csh -c '$CA'\" > /dev/null"  > command1.sh
 source command1.sh
 
 #Start the compute node 2
-CA=`echo "$setenvstring cd $PROJ_PATH &&  nohup $NODE_2_COMMAND & "`
-echo "ssh -t -t -n -f nguy4068@$NODE_2_IP  \"csh -c '$CA'\" > /dev/null"  > command2.sh
+CA=`echo "$setenvstring cd $PROJ_PATH &&  nohup $NODE_COMMAND_2 & "`
+echo "ssh -t -n -f $USERID@$NODE_2_IP  \"csh -c '$CA'\" > /dev/null"  > command2.sh
 source command2.sh
 
 #Start the compute node 3
-CA=`echo "$setenvstring cd $PROJ_PATH && nohup $NODE_3_COMMAND & "`
-echo "ssh -t -t -n -f nguy4068@$NODE_3_IP  \"csh -c '$CA'\" > /dev/null"  > command3.sh
+CA=`echo "$setenvstring cd $PROJ_PATH && nohup $NODE_COMMAND_3 & "`
+echo "ssh -t -n -f $USERID@$NODE_3_IP  \"csh -c '$CA'\" > /dev/null"  > command3.sh
 source command3.sh
 
 # Sleep for a few seconds so the servers can startup
@@ -84,7 +84,7 @@ echo "The compute nodes should have booted up by now"
 
 #Start the server
 CA=`echo "$setenvstring cd $PROJ_PATH &&  nohup $SERVER_COMMAND & "`
-echo "ssh -t -t -n -f nguy4068@$SERVER_IP  \"csh -c '$CA'\" > /dev/null"  > command4.sh
+echo "ssh -t -n -f $USERID@$SERVER_IP  \"csh -c '$CA'\" > /dev/null"  > command4.sh
 source command4.sh
 
 # Sleep for a few seconds so the server can startup 
@@ -94,39 +94,39 @@ echo "The server should have booted up by now"
 
 #Start the client. It should automatically send the request
 CA=`echo "$setenvstring cd $PROJ_PATH && nohup $CLIENT_COMMAND & "`
-echo "ssh -t -t -n -f nguy4068@$CLIENT_IP  \"csh -c '$CA'\" > /dev/null"  > command5.sh
+echo "ssh -t -n -f $USERID@$CLIENT_IP  \"csh -c '$CA'\" > /dev/null"  > command5.sh
 source command5.sh
 
 ##### Assign Points ####
-echo "ssh -t -t -n -f nguy4068@$SERVER_IP  ' ps -ef  > log_0  ' "  > grade0.sh
-echo "ssh -t -t -n -f nguy4068@$CLIENT_IP  ' ps -ef > log_1  ' "  > grade1.sh
-echo "ssh -t -t -n -f nguy4068@$NODE_0_IP  ' ps -ef  > log_2  ' "  > grade2.sh
-echo "ssh -t -t -n -f nguy4068@$NODE_1_IP  ' ps -ef  > log_3  ' "  > grade3.sh
-echo "ssh -t -t -n -f nguy4068@$NODE_2_IP  ' ps -ef > log_4  ' "  > grade4.sh
-echo "ssh -t -t -n -f nguy4068@$NODE_3_IP  ' ps -ef > log_5  ' "  > grade5.sh
+echo "ssh -t -n -f $USERID@$SERVER_IP  ' ps -ef  > log_0  ' "  > grade0.sh
+echo "ssh -t -n -f $USERID@$CLIENT_IP  ' ps -ef > log_1  ' "  > grade1.sh
+echo "ssh -t -n -f $USERID@$NODE_0_IP  ' ps -ef  > log_2  ' "  > grade2.sh
+echo "ssh -t -n -f $USERID@$NODE_1_IP  ' ps -ef  > log_3  ' "  > grade3.sh
+echo "ssh -t -n -f $USERID@$NODE_2_IP  ' ps -ef > log_4  ' "  > grade4.sh
+echo "ssh -t -n -f $USERID@$NODE_3_IP  ' ps -ef > log_5  ' "  > grade5.sh
 
-echo "Waiting 60s for logging to complete ..."
-sleep 60
 score=0
 for i in 0 1 2 3 4 5 
 do
     source grade$i.sh
+    echo "Waiting 120s for logging to complete ..."
     sleep 10
     if [ $i -eq 0 ]; then 
-      numlines=`cat ~/log_$i | grep "$SERVER_PROGRAM" | wc -l`
+      numlines=`cat ~/log_$i | grep "$SERVER_COMMAND" | wc -l`
     elif [ $i -eq 1 ]; then
-      numlines=`cat ~/log_$i | grep "$CLIENT_PROGRAM" | wc -l`
+      numlines=`cat ~/log_$i | grep "$CLIENT_COMMAND" | wc -l`
     elif [ $i -eq 2 ]; then
-      numlines=`cat ~/log_$i | grep "$NODE_0_PROGRAM" | wc -l`
+      numlines=`cat ~/log_$i | grep "$NODE_0_COMMAND" | wc -l`
     elif [ $i -eq 3 ]; then
-      numlines=`cat ~/log_$i | grep "$NODE_1_PROGRAM" | wc -l`
+      numlines=`cat ~/log_$i | grep "$NODE_1_COMMAND" | wc -l`
     elif [ $i -eq 4 ]; then
-      numlines=`cat ~/log_$i | grep "$NODE_2_PROGRAM" | wc -l`
+      numlines=`cat ~/log_$i | grep "$NODE_2_COMMAND" | wc -l`
     elif [ $i -eq 5 ]; then
-      numlines=`cat ~/log_$i | grep "$NODE_3_PROGRAM" | wc -l`
+      numlines=`cat ~/log_$i | grep "$NODE_3_COMMAND" | wc -l`
     fi
 
     if [ $numlines -gt 1  ]; then
+       echo "adding +2 ..."
        score=`echo "scale=0;$score + 2 " | bc `
     fi
 done
@@ -134,9 +134,10 @@ done
 echo "Waiting for 60s for image processing to be completed ..."
 sleep 10 
 input_lines=`ls -la $PROJ_PATH/input_dir/ | wc -l`
-output_lines=`ls -la output_dir/ | wc -l`
+output_lines=`ls -la $PROJ_PATH/output_dir/ | wc -l`
 if [ ! $input_lines -gt $output_lines  ]; then
     if [ $output_lines -gt 3 ]; then 
+         echo "adding +3 ..."
          score=`echo "scale=0;$score + 3" | bc`
     fi
 fi
---
+++ autograder/kill_command_1.sh
@@ -0,0 +1,1 @@
+ssh -t -n -f kim00967@kh4250-08.cselabs.umn.edu  'pkill -f ".*ant -q -Dcomputenodeid=1 computenodeserver.*"'
