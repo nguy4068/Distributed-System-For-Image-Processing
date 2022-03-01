780f0361cd10e79a8a1a1e8eb8ce7f310afb5961
Joon Kim <kim00967@umn.edu>
2022-02-28 23:46:40 -0600

    Reflect the grading scripts changes
    This commit reflects the changes that were made by the TA.
    Fix the typos in `grading.sh`.

--- autograder/cleanup.sh
+++ autograder/cleanup.sh
@@ -38,15 +38,24 @@ while read p; do
    count=`echo "scale=0;$count + 1" | bc`
 done < commands.txt
 
+jcount=`echo $NODE_COMMAND_0 | grep "/java" | wc -l`
+if [ $jcount -gt 0 ]; then
+   SERVER_COMMAND="/java"
+   CLIENT_COMMAND="/java"
+   NODE_COMMAND_0="/java"
+   NODE_COMMAND_1="/java"
+   NODE_COMMAND_2="/java"
+   NODE_COMMAND_3="/java"
+fi
 
 USERID=`whoami`
 
-echo "ssh -t -n -f $USERID@$NODE_0_IP  'pkill -f \".*$NODE_COMMAND_0.*\"'" > kill_command_0.sh
-echo "ssh -t -n -f $USERID@$NODE_1_IP  'pkill -f \".*$NODE_COMMAND_1.*\"'" > kill_command_1.sh
-echo "ssh -t -n -f $USERID@$NODE_2_IP  'pkill -f \".*$NODE_COMMAND_2.*\"'" > kill_command_2.sh
-echo "ssh -t -n -f $USERID@$NODE_3_IP  'pkill -f \".*$NODE_COMMAND_3.*\"'" > kill_command_3.sh
-echo "ssh -t -n -f $USERID@$SERVER_IP  'pkill -f \".*$SERVER_COMMAND.*\"'" > kill_command_4.sh
-echo "ssh -t -n -f $USERID@$CLIENT_IP  'pkill -f \".*$CLIENT_COMMAND.*\"'" > kill_command_5.sh
+echo "ssh -t -n -f $USERID@$NODE_0_IP  'pkill -f \".*$NODE_COMMAND_0.*\"' -U $USERID" > kill_command_0.sh
+echo "ssh -t -n -f $USERID@$NODE_1_IP  'pkill -f \".*$NODE_COMMAND_1.*\"' -U $USERID" > kill_command_1.sh
+echo "ssh -t -n -f $USERID@$NODE_2_IP  'pkill -f \".*$NODE_COMMAND_2.*\"' -U $USERID" > kill_command_2.sh
+echo "ssh -t -n -f $USERID@$NODE_3_IP  'pkill -f \".*$NODE_COMMAND_3.*\"' -U $USERID" > kill_command_3.sh
+echo "ssh -t -n -f $USERID@$SERVER_IP  'pkill -f \".*$SERVER_COMMAND.*\"' -U $USERID" > kill_command_4.sh
+echo "ssh -t -n -f $USERID@$CLIENT_IP  'pkill -f \".*$CLIENT_COMMAND.*\"' -U $USERID" > kill_command_5.sh
 
 source kill_command_0.sh
 source kill_command_1.sh
--- autograder/grading.sh
+++ autograder/grading.sh
@@ -105,6 +105,16 @@ echo "ssh -t -n -f $USERID@$NODE_1_IP  ' ps -ef  > log_3  ' "  > grade3.sh
 echo "ssh -t -n -f $USERID@$NODE_2_IP  ' ps -ef > log_4  ' "  > grade4.sh
 echo "ssh -t -n -f $USERID@$NODE_3_IP  ' ps -ef > log_5  ' "  > grade5.sh
 
+jcount=`echo $NODE_COMMAND_0 | grep "/java" | wc -l`
+if [ $jcount -gt 0 ]; then
+   SERVER_COMMAND="/java"
+   CLIENT_COMMAND="/java"
+   NODE_COMMAND_0="/java"
+   NODE_COMMAND_1="/java"
+   NODE_COMMAND_2="/java"
+   NODE_COMMAND_3="/java"
+fi
+
 score=0
 for i in 0 1 2 3 4 5 
 do
@@ -116,16 +126,16 @@ do
     elif [ $i -eq 1 ]; then
       numlines=`cat ~/log_$i | grep "$CLIENT_COMMAND" | wc -l`
     elif [ $i -eq 2 ]; then
-      numlines=`cat ~/log_$i | grep "$NODE_0_COMMAND" | wc -l`
+      numlines=`cat ~/log_$i | grep "$NODE_COMMAND_0" | wc -l`
     elif [ $i -eq 3 ]; then
-      numlines=`cat ~/log_$i | grep "$NODE_1_COMMAND" | wc -l`
+      numlines=`cat ~/log_$i | grep "$NODE_COMMAND_1" | wc -l`
     elif [ $i -eq 4 ]; then
-      numlines=`cat ~/log_$i | grep "$NODE_2_COMMAND" | wc -l`
+      numlines=`cat ~/log_$i | grep "$NODE_COMMAND_2" | wc -l`
     elif [ $i -eq 5 ]; then
-      numlines=`cat ~/log_$i | grep "$NODE_3_COMMAND" | wc -l`
+      numlines=`cat ~/log_$i | grep "$NODE_COMMAND_3" | wc -l`
     fi
 
-    if [ $numlines -gt 1  ]; then
+    if [ $numlines -gt 0  ]; then
        echo "adding +2 ..."
        score=`echo "scale=0;$score + 2 " | bc `
     fi
